2558a2a8039f05fb2ed56f90cd9274ffcddf2ef8
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-07 09:26:56 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: return 0 for successful execution
+*/
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-
 	return (0);
-
 }
 
