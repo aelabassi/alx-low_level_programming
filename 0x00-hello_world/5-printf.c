4763110f3325a9dff6776c88e08d5fa9821b1b93
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-07 10:35:08 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints a string
  *
- * Return: return 0 for successfull execution 
+ * Return: returns 0 for  successfull execution
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
