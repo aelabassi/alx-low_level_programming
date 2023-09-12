cb5cb4e85f0834c96651a530d76953f39d292585
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-12 14:36:53 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,18 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
+	{
 		n = -n;
 		a = n % 10;
-
+	}
 	if (a < 0)
+	{
 		a = a * -1;
 		_putchar(a + '0');
 		return (a);
+	}
+	a = n % 10;
+	_putchar(a + '0');
+	return (a);
 
 }
