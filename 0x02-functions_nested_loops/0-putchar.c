8063d3ac0f3dae05f0bbcbfa4b8b9a4440fde27e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-12 08:46:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.c"
 #include <string.h>
 /**
  * main - prints _putchar
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	char p[7] = "_putchar";
+	char p[] = "_putchar";
 
 	_putchar(p);
 
