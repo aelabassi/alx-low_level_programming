25f276df86f7c539e880e9e9868a99a93c455f21
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-12 07:13:56 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - prints _putchar
  *
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+	-putchar("_putchar\n");
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar - prints a string
+ * @c: array of characters
+*/
+void _putchar(char c[])
+{
+	printf("%c\n", c);
+
+	return (0);
+}
+
+
+
+
+#endif /* _MAIN_H */
