292ea544689d58844f7192c98c400efb0569dea0
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-13 01:43:30 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
 				if (j == 0)
@@ -21,9 +21,7 @@ void print_times_table(int n)
 				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					printf("%s%s%s", " ", " ", " ");
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
