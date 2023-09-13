e02577cf3926f015324655e06626548d22b1683a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-13 01:53:32 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,7 @@ void print_times_table(int n)
 				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
-					printf("%s%s%s", " ", " ", " ");
+					_putchar(' '), _putchar(' '), _putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
