1542f9d81f1d6a54f8f35ba1c435dcd25a0010af
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-13 09:23:51 -0700

    first debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test print_or_negative function
+ *
+ * Return: Always 0.
+*/
+void main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
