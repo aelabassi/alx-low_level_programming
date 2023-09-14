f5412c6d2fcf393d3bb50795e87282a1aec26e18
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-14 09:29:25 -0700

    multiply two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - muliplies two integers
+ *@a: first int
+ *@b: second int
+ * Return: returns an int
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
 
 int _isdigit(int c);
 int _isupper(int c);
-
+int mul(int a, int b);
 
 
 
