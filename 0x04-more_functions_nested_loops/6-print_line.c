4fcf3ade48b70c178f6d76c1f0be08443f59989d
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-14 13:10:52 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,12 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+	int i;
 
-		for (i = 0; i < n; i++)
-		{
-			_putchar('-');
-		}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
+
+	_putchar('\n');
 }
