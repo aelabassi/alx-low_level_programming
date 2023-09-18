922474146043b08aba8e1e7d002cac2790d87879
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-18 10:49:09 -0700

    print  the last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - print the last half of a string
+ *@str: pointer to a char
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n = strlen(str) - 1;
+
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
