47d87300073ee2f757e6ee3c403fbccf00a86f64
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-18 11:01:54 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - prints the elements of an array
+ *@a: pointer to an integer, arrays of integers
+ *@n: int
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
