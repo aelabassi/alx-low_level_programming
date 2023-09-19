5270ed7f5d6a69a318b3e89444156cbd94ae2c4c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-19 09:48:14 -0700

    convert a string to an int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 	int i = 0;
 	unsigned int conv = 0;
 
-	while ((s[i] >= '0' && s[i] <= '9') && (s[i] != '\0'))
+	while (!(s[i] >= '0' && s[i] <= '9') && (s[i] != '\0'))
 	{
 		if (s[i] == '-')
 		{
