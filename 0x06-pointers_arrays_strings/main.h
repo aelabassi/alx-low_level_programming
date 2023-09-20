efa765639087b59de5b2e604ecc5d578834eb8ad
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-09-20 10:01:34 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* MAIN_H */
