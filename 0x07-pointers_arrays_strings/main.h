49e3db039a18906475bb6013cbb4da4bef0f9c94
Abdoelabassi <abderrezaq@outlook.com>
2023-09-25 18:47:24 +0100

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* MAIN_H */
