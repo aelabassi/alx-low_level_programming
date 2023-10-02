fd0e29d70b6f9c43ff0bedd20ffa751a96bb6614
Abdoelabassi <abderrezaq@outlook.com>
2023-10-02 21:15:03 +0100

    first argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *@argc: number of command line arguments
+ *@argv: command line arguments
+ * Return: Always 0.
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
