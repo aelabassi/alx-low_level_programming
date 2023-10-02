396a1fbfbf34fb7eb9efccd97d203e45fa438bb7
Abdoelabassi <abderrezaq@outlook.com>
2023-10-02 22:00:32 +0100

    multiply two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: int, number of arguments
+ * @argv: char, arguments
+ * Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int n, m, prod;
+
+	if (argc != 3)
+	{
+		printf("Must have 3 arguments!\n");
+	}
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	prod = n * m;
+	printf("%d\n", prod);
+
+	return (0);
+
+
+}
