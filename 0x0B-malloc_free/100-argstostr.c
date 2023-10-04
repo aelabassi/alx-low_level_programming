78074b35424b3844eff7fb2cb2c0db1253914a9e
Abdoelabassi <abderrezaq@outlook.com>
2023-10-04 20:03:31 +0100

    malloc advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,8 +11,7 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j;
-	size_t size = strlen(*av);
+	int i, j, k, size;
 	char *s;
 
 	if (ac == 0)
@@ -21,29 +20,42 @@ char *argstostr(int ac, char **av)
 	}
 	else
 	{
-		s = malloc(size * sizeof(*av) + 1);
-		if (s == NULL)
-		{
-			free(s);
-			return (NULL);
-		}
 
-		for (i = 0; i < ac; i++)
+
+		for (size = i = 0; i < ac; i++)
 		{
 			if (av[i] == NULL)
 			{
 				return (NULL);
 			}
 
-			for (j = 0; av[i][j]; j++)
+			for (j = 0; av[i][j] != '\0'; j++)
 			{
-				if (av[i][j] == '\0')
-				{
-					s[j] = '\n';
-				}
-				s[j] = av[i][j];
+				size++;
+			}
+			size++;
+		}
+		s = malloc((size + 1) * sizeof(char));
+		if (s == NULL)
+		{
+			free(s);
+			return (NULL);
+		}
+		for (i = j = k = 0; k < size; j++, k++)
+		{
+			if (av[i][j] == '\0')
+			{
+				s[k] = '\n';
+				i++;
+				k++;
+				j = 0;
+			}
+			if (k < size - 1)
+			{
+				s[k] = av[i][j];
 			}
 		}
+		s[k] = '\0';
 	}
 
 	return (s);
