c2941b5c62ed315a7397630f2d7dd58ae73edee2
Abdoelabassi <abderrezaq@outlook.com>
2023-10-04 20:27:08 +0100

    malloc advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,43 +20,35 @@ char *argstostr(int ac, char **av)
 	}
 	else
 	{
-
-
-		for (size = i = 0; i < ac; i++)
-		{
-			if (av[i] == NULL)
-			{
-				return (NULL);
-			}
-
-			for (j = 0; av[i][j] != '\0'; j++)
-			{
-				size++;
-			}
-			size++;
-		}
-		s = malloc((size + 1) * sizeof(char));
-		if (s == NULL)
-		{
-			free(s);
+	for (size = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
 			return (NULL);
-		}
-		for (i = j = k = 0; k < size; j++, k++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = j = k = 0; k < size; j++, k++)
+	{
+		if (av[i][j] == '\0')
 		{
-			if (av[i][j] == '\0')
-			{
-				s[k] = '\n';
-				i++;
-				k++;
-				j = 0;
-			}
-			if (k < size - 1)
-			{
-				s[k] = av[i][j];
-			}
+			s[k] = '\n';
+			i++;
+			k++;
+			j = 0;
+		}
+		if (k < size - 1)
+		{
+			s[k] = av[i][j];
 		}
 		s[k] = '\0';
 	}
-
+	}
 	return (s);
 }
