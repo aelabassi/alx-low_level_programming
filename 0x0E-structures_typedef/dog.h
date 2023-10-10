a4769b8b891ea1e3d4009ce6dd0d8df0a4f5cf80
Abdoelabassi <abderrezaq@outlook.com>
2023-10-10 22:59:10 +0100

    new typedef for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,11 @@ struct dog
 	char *owner;
 } dog;
 
+/**
+ * dog_t - typdef for structure dog
+*/
+typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
