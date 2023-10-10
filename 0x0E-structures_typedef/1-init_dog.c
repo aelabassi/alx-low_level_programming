6116b3302b862e67234b689ff0ceadcfd2d3d4d2
Abdoelabassi <abderrezaq@outlook.com>
2023-10-10 22:26:44 +0100

    init struct's attributes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize the structure dog's attributes
@@ -10,8 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+	    d->age = age;
+	    d->owner = owner;
+	}
 
 }
