2bc2ae6bc61b5d1dd20347dff3bc6e32e3884712
Abdoelabassi <abderrezaq@outlook.com>
2023-10-12 00:17:21 +0100

    test test..

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,14 +13,20 @@ int main(int argc, char *argv[])
 	int (*op)(int, int);
 
 	if (argc != 4)
+	{
 		printf("Error\n");
 		exit(89);
+	}
+
 
 	op = get_op_func(argv[2]);
 
 	if (!op)
+	{
 		printf("Error\n");
 		exit(99);
+	}
+
 
 	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 	return (0);
