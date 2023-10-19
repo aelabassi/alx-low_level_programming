4276cb1bfe64ed4e6847b60bdbf1cf45585cf5c9
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-19 21:58:43 +0100

    print a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 	while (h == NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] (nil)", 0);
+			printf("[%d] (nil)\n", 0);
 		else
-		    printf("[%u] %s", h->len, h->str);
+		    printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		el++;
