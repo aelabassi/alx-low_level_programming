ea0bfbcc3acb5c99f9e30b6c0a70d58335fcc9c3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-19 21:54:21 +0100

    print a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,9 +20,9 @@ size_t print_list(const list_t *h);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 #endif
