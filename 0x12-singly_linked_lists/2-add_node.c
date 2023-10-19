e23983d76cb177dccb71c022d7fad19ed931f7bc
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-19 22:46:52 +0100

    add  a new element to linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ * of list_t
+ * @head: pointer to pointer to list_t
+ * @str: pointer to const char
+ * Return: address of the new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
+}
