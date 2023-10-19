faf15bda2112d3ae50386b0634c4a4c7c9fabd95
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-19 21:50:02 +0100

    print a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of list_t
+ * @h: pointer to const list_t
+ * Return: elements of list_t of size_t type
+*/
+size_t print_list(const list_t *h)
+{
+	size_t el = 0;
+
+	while (h == NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil)", 0);
+		printf("[%u] %s", h->len, h->str);
+		h = h->next;
+		el++;
+	}
+	return (el);
+}
