5baeaa71e280a4b539292eb8ce64e891c210414a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-23 22:26:16 +0100

    print the elements of linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_listint - prints all elments of a listint_t
+ * @h: pointer to listint
+ * Return: all elements, size_t type
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t elem = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elem++;
+	}
+
+	return (elem);
+}
