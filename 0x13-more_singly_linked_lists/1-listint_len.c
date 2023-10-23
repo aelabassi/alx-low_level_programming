d6730a69f3142a16b23be11b185feae4d4f8961b
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-10-23 23:03:02 +0100

    nuber of the elements in the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * listint_len - retuns the number of elements in the linked list
+ * @h: pointer to const lisrint
+ * Return: size_t
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (h);
+}
