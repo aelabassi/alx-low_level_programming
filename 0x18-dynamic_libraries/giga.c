f9a5562a3e9560fe4bced9202a4af92307abfb4a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-15 16:31:48 +0100

    operations shared lib on the way..

--- 0x18-dynamic_libraries/giga.c
+++ 0x18-dynamic_libraries/giga.c
@@ -2,41 +2,30 @@
 #include <unistd.h>
 
 /**
- * giga - runs a giga game to win a jackpot
- *
- * Return: int
- */
-int giga()
+* giga - runs a giga game to win a jackpot
+*
+* Return: int
+*/
+int giga(void)
 {
 	static int pick = -1;
 
 	pick++;
 
-	switch(pick)
-	{
-		case 0:
-			return 8;
-			break;
-		case 1:
-			return 8;
-			break;
-		case 2:
-			return 7;
-			break;
-		case 3:
-			return 9;
-			break;
-		case 4:
-			return 23;
-			break;
-		case 5:
-			return 74;
-			break;
-		default:
-			return 0;
-			break;
-	}
-	return pick * pick % 30000;
+	if (pick == 0)
+		return (8);
+	if (pick == 1)
+		return (8);
+	if (pick == 2)
+		return (7);
+	if (pick == 3)
+		return (9);
+	if (pick == 4)
+		return (23);
+	if (pick == 5)
+		return (74);
+
+	return (pick * pick % 30000);
 
 }
 
