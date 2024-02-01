8e0914e83358b77bef47f23f5b10d12cfda8049e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-02 00:28:48 +0100

    djb2 hash function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function based on djb2 algorithm
+ * @str: (uchar)
+ * Return: the hash function
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
+}
+
