979c44f259e921999a52985d7da9cb1cd54e19c3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-28 11:50:22 +0100

    rot13 cipher

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,24 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
- * rot13 - rotate a letter in text 13 position
- *@s: pointer to char
- * Return: pointer to char
- */
+* rot13 - rotate a letter in text 13 position
+*@s: pointer to char
+* Return: pointer to char
+*/
 char *rot13(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		if (isalpha(s[i]))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-			{
 				s[i] += 13;
-			}
 			else
-			{
 				s[i] -= 13;
-			}
-			i++;
 		}
-		i++;
 	}
 	return (s);
 }
